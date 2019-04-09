78aa0caa8fc50c39ca0df126a3d1391542cb3e80
Yu-Song Syu <kuma@simbo.com.tw>
2019-04-09 23:36:51 +0800

    predicator

---
+++ src/main/java/idv/kuma/PredicatorExamples.java
@@ -0,0 +1,36 @@
+package idv.kuma;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * Hello world!
+ */
+public class PredicatorExamples {
+
+
+    public static void main(String[] args) {
+
+
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+
+        List<Integer> evenNumbers = filter(numbers, i -> i % 2 == 0);
+
+
+        System.out.println(evenNumbers);
+    }
+
+    public static <T> List<T> filter(List<T> list, Predicate<T> p) {
+        List<T> result = new ArrayList<>();
+
+        for (T t : list) {
+            if (p.test(t)) {
+                result.add(t);
+            }
+        }
+        return result;
+
+    }
+}
