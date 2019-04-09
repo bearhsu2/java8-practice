9f624b49f1c296ddedc3ba805aae1a768d876802
Yu-Song Syu <kuma@simbo.com.tw>
2019-04-10 00:23:52 +0800

    lambda

--- src/main/java/idv/kuma/PredicateExamples.java
+++ src/main/java/idv/kuma/PredicateExamples.java
@@ -4,11 +4,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
  * Hello world!
  */
-public class HandMakePredicatorExamples {
+public class PredicateExamples {
 
 
     public static void main(String[] args) {
@@ -17,9 +18,16 @@ public class HandMakePredicatorExamples {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
 
         List<Integer> evenNumbers = filter(numbers, i -> i % 2 == 0);
-
-
         System.out.println(evenNumbers);
+
+
+        List<String> strings = Arrays.asList("aa", "", "cc", "", "ee");
+        List<String> nonEmptyStrings = filter(strings, s -> !s.isEmpty());
+        System.out.println(nonEmptyStrings);
+
+
+        System.out.println(strings.stream().filter( s -> !s.isEmpty()).collect(Collectors.toList()));
+
     }
 
     public static <T> List<T> filter(List<T> list, Predicate<T> p) {
