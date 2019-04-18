af953d6ad375e9ae49d311ca0f9468215751cd81
Yu-Song Syu <kuma@simbo.com.tw>
2019-04-18 23:11:32 +0800

    lambda invoke constructor

--- src/main/java/idv/kuma/MethodInvokeExample.java
+++ src/main/java/idv/kuma/MethodInvokeExample.java
@@ -1,15 +1,28 @@
 package idv.kuma;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 
 public class MethodInvokeExample {
 
+    static Map<String, BiFunction<Integer, String, Apple>> map = new HashMap<>();
+
+    static {
+        map.put("apple", Apple::new);
+    }
+
+    public static Apple createApple(String name, Integer weight, String color) {
+        return map.get(name).apply(weight, color);
+    }
+
     public static void main(String[] args) {
 
         List<String> strings1 = Arrays.asList("12", "123");
-        List<Integer> collect = strings1.stream().map(s -> Integer.parseInt(s)).collect(Collectors.toList());
+        List<Integer> collect = strings1.stream().map(Integer::parseInt).collect(Collectors.toList());
         System.out.println(collect);
 
 
@@ -18,9 +31,11 @@ public class MethodInvokeExample {
         System.out.println(strings2);
 
         List<String> strings3 = Arrays.asList("aA", "bSF", "AA", "cC");
-        List<String> lowerCases = strings3.stream().map(s -> s.toLowerCase()).collect(Collectors.toList());
+        List<String> lowerCases = strings3.stream().map(String::toLowerCase).collect(Collectors.toList());
         System.out.println(lowerCases);
 
 
+        Apple apple = createApple("apple", 34, "red");
+        System.out.println(apple);
     }
 }
