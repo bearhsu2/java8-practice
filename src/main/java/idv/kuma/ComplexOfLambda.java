beebc6d11529c29e8a790bc062369f873a13f886
Yu-Song Syu <kuma@simbo.com.tw>
2019-04-25 00:22:27 +0800

    complex predicate

--- src/main/java/idv/kuma/ComplexOfLambda.java
+++ src/main/java/idv/kuma/ComplexOfLambda.java
@@ -3,13 +3,17 @@ package idv.kuma;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class ComplexOfLambda {
 
 
     public static void main(String[] args) {
+        List<Apple> inventory;
 
-        List<Apple> inventory = Arrays.asList(new Apple(10, "Green"),
+        // Comparator
+        inventory = Arrays.asList(new Apple(10, "Green"),
                 new Apple(13, "Red"),
                 new Apple(3, "Yellow"),
                 new Apple(3, "Black"));
@@ -21,6 +25,22 @@ public class ComplexOfLambda {
                 .reversed()
                 .thenComparing(Apple::getColor));
         System.out.println(inventory);
+
+        // Predicate
+        inventory = Arrays.asList(new Apple(10, "Green"),
+                new Apple(13, "Red"),
+                new Apple(50, "Red"),
+                new Apple(3, "Yellow"),
+                new Apple(3, "Black"));
+
+        Predicate<Apple> redApple = a -> "Red".equalsIgnoreCase(a.getColor());
+
+        Predicate<Apple> notRedAndHeavyOrBlack = redApple.negate().and(a -> a.getWeight() > 5).or(a -> a.getColor().equalsIgnoreCase("Black"));
+
+        System.out.println(inventory.stream().filter(notRedAndHeavyOrBlack).collect(Collectors.toList()));
+
+        
+
     }
 
 
