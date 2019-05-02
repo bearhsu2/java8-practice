8c45c528acf41914fe63420607e143e0b588e882
Yu-Song Syu <kuma@simbo.com.tw>
2019-05-02 23:15:47 +0800

    ok

--- src/main/java/idv/kuma/stream/numbers/NumberStreams.java
+++ src/main/java/idv/kuma/stream/numbers/NumberStreams.java
@@ -3,9 +3,11 @@ package idv.kuma.stream.numbers;
 import idv.kuma.stream.Dish;
 import idv.kuma.stream.Menu;
 
-import java.beans.IntrospectionException;
+import java.util.Arrays;
 import java.util.OptionalInt;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class NumberStreams {
 
@@ -20,12 +22,21 @@ public class NumberStreams {
         System.out.println(optionalMax.orElse(-1));
 
 
-
         IntStream evenStream = IntStream.rangeClosed(1, 100).filter(i -> i % 2 == 0);
         System.out.println(evenStream.count());
 
         System.out.println(IntStream.rangeClosed(1, 100).max());
         System.out.println(IntStream.range(1, 100).max());
+
+        Stream<int[]> stream = IntStream.rangeClosed(1, 100).boxed().flatMap(a ->
+                IntStream.rangeClosed(a, 100)
+                        .filter(b -> Math.sqrt(a * a + b * b) % 1 == 0)
+                        .mapToObj(b ->
+                                new int[]{a, b, (int) Math.sqrt(a * a + b * b)}));
+        stream.limit(5)
+                .forEach(t -> System.out.println(Arrays.stream(t)
+                        .mapToObj(String::valueOf)
+                        .collect(Collectors.joining(","))));
     }
 
 
