23ef13ce2f60f93e86c6c961021baefb94530976
Yu-Song Syu <kuma@simbo.com.tw>
2019-05-04 10:37:27 +0800

    create from array

--- src/main/java/idv/kuma/stream/CreatingStreams.java
+++ src/main/java/idv/kuma/stream/CreatingStreams.java
@@ -1,15 +1,21 @@
 package idv.kuma.stream;
 
 
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class CreatingStreams {
 
     public static void main(String[] args) {
 
-        // create from given values
+        // 1. create from given values
         Stream<String> stringStream = Stream.of("I", "Love", "Java", "8", "lar", "!");
         stringStream.map(String::toUpperCase).forEach(System.out::println);
+
+        // 2. create from array
+        int[] numbers = {1, 4, 2, 6, 9, 3, 7, 0};
+        int sum = Arrays.stream(numbers).sum();
+        System.out.println(sum);
 
     }
 }
