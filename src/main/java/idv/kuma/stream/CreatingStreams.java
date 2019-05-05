628043e6878fffe3cfdb5682eb297b943307d818
Yu-Song Syu <kuma@simbo.com.tw>
2019-05-05 18:50:09 +0800

    create from  file I/O

--- src/main/java/idv/kuma/stream/CreatingStreams.java
+++ src/main/java/idv/kuma/stream/CreatingStreams.java
@@ -1,10 +1,15 @@
 package idv.kuma.stream;
 
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class CreatingStreams {
+
 
     public static void main(String[] args) {
 
@@ -17,5 +22,18 @@ public class CreatingStreams {
         int sum = Arrays.stream(numbers).sum();
         System.out.println(sum);
 
+        // 3. create from file I/O
+        try (Stream<String> lines = Files.lines(Paths.get("data.txt"), Charset.defaultCharset())) {
+
+            long count = lines.flatMap(l -> Arrays.stream(l.split(" ")))
+                    .distinct()
+                    .count();
+            System.out.println(count);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+
     }
 }
+
