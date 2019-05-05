a6fcc9e3febff86e76407273a5cc98db071073bd
Yu-Song Syu <kuma@simbo.com.tw>
2019-05-05 22:52:43 +0800

    ok

--- src/main/java/idv/kuma/collector/CollectorsExamples.java
+++ src/main/java/idv/kuma/collector/CollectorsExamples.java
@@ -1,22 +1,39 @@
 package idv.kuma.collector;
 
 import idv.kuma.stream.Dish;
-import idv.kuma.stream.Menu;
 
+import java.util.Comparator;
 import java.util.stream.Collectors;
+
+import static idv.kuma.stream.Menu.menu;
 
 public class CollectorsExamples {
 
     public static void main(String[] args) {
-        Integer sum1 = Menu.menu.stream()
+
+        // sum
+        Integer sum1 = menu.stream()
                 .map(d -> d.getCalories())
                 .reduce(0, (a, b) -> a + b);
         System.out.println(sum1);
 
-        Integer sum2 = Menu.menu.stream().collect(Collectors.summingInt(Dish::getCalories));
+        Integer sum2 = menu.stream().collect(Collectors.summingInt(Dish::getCalories));
         System.out.println(sum2);
 
-        Integer sum3 = Menu.menu.stream().mapToInt(Dish::getCalories).sum();
+        Integer sum3 = menu.stream().mapToInt(Dish::getCalories).sum(); // the most recommended!!!
         System.out.println(sum3);
+
+        Integer sum4 = menu.stream().collect(Collectors.reducing(0, Dish::getCalories, Integer::sum));
+        System.out.println(sum4);
+
+
+        // min/max
+        System.out.println(menu.stream().min(Comparator.comparingInt(Dish::getCalories)).get());
+        System.out.println(menu.stream().collect(Collectors.minBy(Comparator.comparingInt(Dish::getCalories))).get());
+
+        // joining
+        System.out.println(menu.stream().map(d -> d.getName()).collect(Collectors.joining(", ")));
+
+
     }
 }
