713b2eeca71efe9c419ff99cff00e7f083efdf5e
Yu-Song Syu <kuma@simbo.com.tw>
2019-05-20 23:26:21 +0800

    the lambda way

--- src/main/java/idv/kuma/refactor/subjectobserver/Main.java
+++ src/main/java/idv/kuma/refactor/subjectobserver/Main.java
@@ -8,11 +8,33 @@ import idv.kuma.refactor.subjectobserver.subject.Feed;
 public class Main {
 
     public static void main(String[] args) {
-        Feed f = new Feed();
-        f.registerObserver(new NYTimes());
-        f.registerObserver(new Guardian());
-        f.registerObserver(new LeMonde());
 
-        f.notifyObservers("The queen said her favorite book is Java 8 in Action!");
+        // The Old Fashioned Way
+        Feed f1 = new Feed();
+        f1.registerObserver(new NYTimes());
+        f1.registerObserver(new Guardian());
+        f1.registerObserver(new LeMonde());
+
+        f1.notifyObservers("The queen said her favorite book is Java 8 in Action!");
+
+        // The Lambda Way
+        Feed f2 = new Feed();
+        f2.registerObserver(tweet -> {
+            if (tweet != null && tweet.contains("money")) {
+                System.out.println("Breaking news in NY! " + tweet);
+            }
+        });
+        f2.registerObserver(tweet -> {
+            if (tweet != null && tweet.contains("queen")) {
+                System.out.println("Yet another news in London... " + tweet);
+            }
+        });
+        f2.registerObserver(tweet -> {
+            if (tweet != null && tweet.contains("wine")) {
+                System.out.println("Today cheese, wine and news! " + tweet);
+            }
+        });
+        f2.notifyObservers("The queen said her favorite book is Java 8 in Action!");
+
     }
 }
