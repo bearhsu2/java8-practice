e7d0c2919e62cd4d51c262f95b75de2bed02b9b2
Yu-Song Syu <kuma@simbo.com.tw>
2019-05-21 23:00:55 +0800

    lambda way

--- src/main/java/idv/kuma/refactor/pattern/Main.java
+++ src/main/java/idv/kuma/refactor/pattern/Main.java
@@ -1,5 +1,8 @@
 package idv.kuma.refactor.pattern;
 
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,9 +11,17 @@ public class Main {
         ProcessingObject<String> p1 = new HeaderTextProcessing();
         ProcessingObject<String> p2 = new SpellCheckProcessing();
         p1.setSuccessor(p2);
-        String result = p1.handle("I really love labda!!!");
-        System.out.println(result);
+        String result1 = p1.handle("I really love labda!!!");
+        System.out.println(result1);
 
         // Lambda way
+        UnaryOperator<String> headerProcessing = (String input) -> "From Jack: " + input;
+        UnaryOperator<String> spellCheckProcessing = (String input) -> input.replaceAll("labda", "lambda");
+        Function<String, String > pipeline =
+                headerProcessing.andThen(spellCheckProcessing);
+        String result2 = pipeline.apply("I really love labda!!!");
+        System.out.println(result2);
+
+
     }
 }
