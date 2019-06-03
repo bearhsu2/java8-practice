b76287c81adcc73fb7236598af02bc92498b59cd
Yu-Song Syu <kuma@simbo.com.tw>
2019-06-03 22:19:35 +0800

    getting values

--- src/main/java/idv/kuma/optional/Main.java
+++ src/main/java/idv/kuma/optional/Main.java
@@ -1,6 +1,7 @@
 package idv.kuma.optional;
 
 import java.util.Optional;
+import java.util.function.IntUnaryOperator;
 
 public class Main {
 
@@ -11,10 +12,32 @@ public class Main {
         Car car = new Car(Optional.of(new Insurance("I1")));
         Optional<Car> optCar = Optional.ofNullable(car);
 
-        String name = optCar.flatMap(c -> c.getInsurance())
-                .map(i -> i.getName())
+        String name = optCar.flatMap(Car::getInsurance)
+                .map(Insurance::getName)
                 .orElse("Unknown");
 
         System.out.println(name);
+
+
+
+
+        // Getting values
+        System.out.println(car.getInsurance().get());
+
+        Car emptyCar = new Car(Optional.ofNullable(null));
+        System.out.println(car.getInsurance().orElse(new Insurance("Default")));
+        System.out.println(emptyCar.getInsurance().orElse(new Insurance("Default")));
+
+        System.out.println(car.getInsurance().orElseGet(() -> new Insurance("Default")));
+        System.out.println(emptyCar.getInsurance().orElseGet(() -> new Insurance("Default")));
+
+        try {
+            emptyCar.getInsurance().orElseThrow(() -> new RuntimeException("hahaha"));
+        } catch (RuntimeException e) {
+            System.out.println(e);
+        }
+
+        car.getInsurance().ifPresent(System.out::println);
+        emptyCar.getInsurance().ifPresent(System.out::println);
     }
 }
