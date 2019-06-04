05786e2779b2c495c781f7b618537b16c931e4f3
Yu-Song Syu <kuma@simbo.com.tw>
2019-06-04 22:38:27 +0800

    future practice

---
+++ src/main/java/idv/kuma/completablefuture/BasicFuture.java
@@ -0,0 +1,35 @@
+package idv.kuma.completablefuture;
+
+import java.util.concurrent.*;
+
+public class BasicFuture {
+
+    public static void main(String[] args) {
+
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+
+        Future<Double> future = executor.submit(new Callable<Double>() {
+            @Override
+            public Double call() throws Exception {
+                Thread.sleep(3000L);
+                return 30D;
+            }
+        });
+
+        System.out.println(50);
+
+        try {
+            Double result = future.get(2, TimeUnit.SECONDS);
+            System.out.println(result);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
