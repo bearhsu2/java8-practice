9de07a63b05d1a30f0b75f55b99b6a5f2e486836
Yu-Song Syu <kuma@simbo.com.tw>
2019-06-09 14:18:03 +0800

    specify executor

--- src/main/java/idv/kuma/completablefuture/completable/Main.java
+++ src/main/java/idv/kuma/completablefuture/completable/Main.java
@@ -3,10 +3,19 @@ package idv.kuma.completablefuture.completable;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 import java.util.stream.Collectors;
 
 public class Main {
     private List<Shop> shops;
+
+    private final Executor executor;
+
+    {
+
+    }
 
     public Main() {
         this.shops = Arrays.asList(
@@ -15,6 +24,18 @@ public class Main {
                 new Shop("MyFavofiteShop"),
                 new Shop("ButItAll")
         );
+
+        executor = Executors.newFixedThreadPool(
+                Math.min(shops.size(), 100),
+                new ThreadFactory() {
+                    @Override
+                    public Thread newThread(Runnable r) {
+                        Thread t = new Thread(r);
+                        t.setDaemon(true);
+                        return t;
+                    }
+                }
+        );
     }
 
     public List<String> findPrices(String product) {
@@ -22,7 +43,7 @@ public class Main {
         List<CompletableFuture<String>> priceFutures = shops.stream()
                 .map(shop ->
                         CompletableFuture.supplyAsync(
-                                () -> shop.getName() + " price is " + shop.getPrice(product)))
+                                () -> shop.getName() + " price is " + shop.getPrice(product), executor))
                 .collect(Collectors.toList());
 
 
@@ -38,6 +59,8 @@ public class Main {
         Main main = new Main();
 
         long start = System.nanoTime();
+
+
         System.out.println(main.findPrices("SSSAAA"));
         long duration = (System.nanoTime() - start) / 1_000_000;
         System.out.println("Done in " + duration + " msecs");
