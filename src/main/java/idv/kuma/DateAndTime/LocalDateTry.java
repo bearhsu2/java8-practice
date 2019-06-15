fdae87d3a8d82acf114cc270c650b04a87fecafe
Yu-Song Syu <kuma@simbo.com.tw>
2019-06-15 20:05:47 +0800

    local time practice

--- src/main/java/idv/kuma/DateAndTime/LocalDateTry.java
+++ src/main/java/idv/kuma/DateAndTime/LocalDateTry.java
@@ -2,18 +2,32 @@ package idv.kuma.DateAndTime;
 
 import java.time.DateTimeException;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.Month;
+import java.time.temporal.ChronoField;
 
 public class LocalDateTry {
 
 
     public static void main(String[] args) {
 
+        // Date
         LocalDate date = LocalDate.of(2019, Month.MAY,3);
         System.out.println(date);
         System.out.println(date.getDayOfWeek());
         System.out.println(date.lengthOfMonth());
         System.out.println(date.isLeapYear());
+        System.out.println(date.get(ChronoField.DAY_OF_MONTH));
+
+        // Time
+        LocalTime  time = LocalTime.of(23,11,22,22);
+        System.out.println(time);
+        System.out.println(time.getMinute());
+
+        // Parse
+        LocalDate parsedDate = LocalDate.parse("2015-03-04");
+        System.out.println(parsedDate);
+
 
 
     }
