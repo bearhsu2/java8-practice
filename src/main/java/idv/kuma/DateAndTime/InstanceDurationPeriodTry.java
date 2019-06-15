f3e6d300271b0a64e731053426ecf5326a0c6c91
Yu-Song Syu <kuma@simbo.com.tw>
2019-06-15 21:59:57 +0800

    hhh

--- src/main/java/idv/kuma/DateAndTime/InstanceDurationPeriodTry.java
+++ src/main/java/idv/kuma/DateAndTime/InstanceDurationPeriodTry.java
@@ -1,4 +1,25 @@
 package idv.kuma.DateAndTime;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.chrono.ChronoLocalDate;
+import java.time.temporal.TemporalAmount;
+
 public class InstanceDurationPeriodTry {
+    public static void main(String[] args) {
+
+        Instant instant1 = Instant.ofEpochSecond(3);
+        Instant instant2 = Instant.ofEpochSecond(4);
+        System.out.println(Duration.between(instant1, instant2));
+
+        System.out.println(Period.between(LocalDate.of(2019,11,13), LocalDate.of(2019,11,14)));
+
+        Duration threeDays = Duration.ofDays(3);
+        Duration newDuration = Duration.from(threeDays);
+
+
+
+    }
 }
